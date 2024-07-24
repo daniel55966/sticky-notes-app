0ed30b01c9974430301d3d5763160076031db652
daniel55966 <daniel55966@gmail.com>
2024-07-23 21:09:31 -0600

    Update vite.config.js with proper repository name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sticky-notes",
+  base: "/sticky-notes-app",
   plugins: [react()],
 })
